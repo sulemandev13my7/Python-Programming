b87b6c27746eac52267d4e400f0eaaa80c18171c
sulemandev13my7 <sulemanafandi18@gmail.com>
2026-06-24 15:13:01 +0500

    update

--- 02Variable.py
+++ 02Variable.py
@@ -1,19 +1,44 @@
-name = "M.Suleman";
-age = 25;
-height = 5.10;
+# name = "M.Suleman";
+# age = 25;
+# height = 5.10;
  
+# name = "Salman";
+
+# print(name)
+# print(age)
+# print(height)
+
+""" print('My name is ' name,'My age is ' age,'My height is ' height) """
+
+# username = "salman"
+# student_id = 123 # snake case
+# _marks = 12
+# phoneNumber = 3492574988 #camel case
+# MyNickName = "salo bahi" #pascal case
+
+# print(username,student_id,_marks,phoneNumber);
+
+# print("Sum is : ",2+5)
+
+# a , b , c,d = 12,13,14,"orange";
+
+# print(a)
+# print(b)
+# print(c)
+# print(a,b,c,d)
+
+# x = y = z = 100;
+# print(x)
+# print(y)
+# print(z)
+
 name = "Salman";
+color = "Red";
 
-print(name)
-print(age)
-print(height)
+print('My name is '+ name + ". My Favourite color is "+ color);
 
-# print('My name is ' name,'My age is ' age,'My height is ' height)
+name = "Salman";
+color = "Red";
+age = 13;
 
-username = "salman"
-student_id = 123 # snake case
-_marks = 12
-phoneNumber = 3492574988 #camel case
-MyNickName = "salo bahi" #pascal case
-
-print(username,student_id,_marks,phoneNumber);
+print('My name is ', name ,"My age is ",age, ". My Favourite color is ", color);
