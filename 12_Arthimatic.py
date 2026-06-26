23cbef5d3f5e3180ba5af29c047b3a20f84f661c
sulemandev13my7 <sulemanafandi18@gmail.com>
2026-06-26 20:27:41 +0500

    update

---
+++ 12_Arthimatic.py
@@ -0,0 +1,23 @@
+a = 12;
+b = 20;
+print(a+b);
+
+a = 12;
+b = 20;
+print(a-b);
+
+a = 12;
+b = 20;
+print(a*b);
+
+a = 12;
+b = 20;
+print(a/b);
+
+a = 12;
+b = 20;
+print(a%b);
+
+a = 20;
+b = 2;
+print(a//b);
