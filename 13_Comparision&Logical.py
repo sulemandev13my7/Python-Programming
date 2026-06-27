a8eb05144a00d22600e409b17a089a7737bb1cd9
sulemandev13my7 <sulemanafandi18@gmail.com>
2026-06-27 20:43:52 +0500

    update

---
+++ 13_Comparision&Logical.py
@@ -0,0 +1,44 @@
+#Comparision Operator
+a = 20;
+b = 30;
+
+print(a>b)
+
+print(a<b)
+
+print(a==b)
+
+print(a!=b)
+
+print(a>=b)
+
+print(a<=b)
+
+a=(10,20,30)
+b=(10,20,30)
+
+
+print(a==b)
+
+age = 18;
+
+print(age >= 18);
+
+#logical Operator
+
+age = 15;
+
+salary = 50000;
+
+print(age >= 13 and salary >= 30000);
+
+
+age = 13;
+
+HasLiecence = False;
+
+print(age >= 13 or HasLiecence >= 30000);
+
+
+age = 20;
+print(not(age <= 20))
