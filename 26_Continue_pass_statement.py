2c9cd83b12673858aa8c9e447267dc6f7b3c58b6
sulemandev13my7 <sulemanafandi18@gmail.com>
2026-07-10 13:24:51 +0500

    update

--- 26_Continue_pass_statement.py
+++ 26_Continue_pass_statement.py
@@ -1,2 +1,40 @@
 for i in range(1,11):
+    if i == 5:
+        continue;
     print(i)
+
+
+for i in range(1,11):
+    if i %2 == 0:
+        continue;
+    print(i)
+    
+li = ['salman','ayan',"",'shif'];    
+for name in li:
+    if name == "":
+        continue;
+    print(name)
+    
+marks = [56,67,87,98,90];
+for mark in marks:
+    if mark < 80:
+        continue;
+    print(mark) 
+    
+
+word = "PYTHON";
+for letter in word:
+    if letter in "AEIOU":
+        continue;
+    print(letter);
+    
+age = 18;
+if age >= 18:
+    pass
+print("You are eligible");
+
+number = [8,9,10];
+for num in number:
+    if num == 9:
+        pass;
+    print(num)
