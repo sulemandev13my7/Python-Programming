0d3ebd6f20ef7f21d1ffb26176845b36ab7b5eea
sulemandev13my7 <sulemanafandi18@gmail.com>
2026-07-19 06:35:56 +0500

    update

---
+++ 27_PathernI.py
@@ -0,0 +1,26 @@
+# Right triangle Pathern  1:
+for a in range(1,10):
+    for b in range(1,a+1):
+        print("*",end=" ");
+    print();
+    
+# Right triangle pathern 2:
+for a in range(1,6):
+    for b in range(1, a + 1):
+        print(b, end=" ");
+    print()
+    
+
+# Right triangle pathern 3:
+for a in range(1,6):
+    for b in range(1,a+1):
+        print(a,end=" ");
+    print()
+    
+# Right triangle pathern 3:
+num=1;
+for a in range(1,6):
+    for b in range(1,a+1):
+        print(num,end=" ")
+        num += 1;
+    print()
---
+++ 28_PathernII.py
@@ -0,0 +1,28 @@
+for a in range(5,0,-1):
+    for b in range(a):
+        print("*",end=" ");
+    print()
+    
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print(j,end=" ");
+    print()
+    
+
+for a in range(5,0,-1):
+    for b in range(a):
+        print(a,end=" ");
+    print();
+    
+for i in range(5,0,-1):
+    for j in range(i,0,-1):
+        print(j,end=" ");
+    print()
+    
+num=1;    
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print(num,end=" ");
+    print()
+    num+1;
+    
