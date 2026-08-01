37e11e94fc6b3ced19bc98f1f9d50240e8c2a3d9
sulemandev13my7 <sulemanafandi18@gmail.com>
2026-08-01 10:41:20 +0500

    update

---
+++ 29_Pathern3.py
@@ -0,0 +1,102 @@
+rows=5;
+for a in range(1,rows+1):
+    for s in range(rows-a):
+        print(" ", end=" ")
+    
+    for b in range(a):
+        print("*",end=" ")
+        
+    print()
+    
+    
+rows=5;
+for a in range(1,rows+1):
+    for s in range(rows-a):
+        print(" ", end="")
+    
+    for b in range(a):
+        print("*",end=" ")
+        
+    print()
+    
+    
+rows=5;
+for a in range(1,rows+1):
+    for s in range(rows-a):
+        print(" ", end=" ")
+    
+    for b in range(1,a+1):
+        print(b,end=" ")
+        
+    print()
+    
+row = 5;
+for a in range(1,row+1):
+    
+    for s in range(row-1):
+        print("",end="")
+        
+    for b in range(1,a+1):
+        print(a,end=" ")
+        
+    print()
+
+rows = 5;
+for a in range(rows,0,-1):
+    
+    for s in range(0,rows - a):
+        print(" ",end=" ");
+        
+    for b in range(a):
+        print("*",end=" ");
+        
+    print()
+    
+rows = 5;
+for a in range(rows,0,-1):
+    
+    for s in range(0,rows - a):
+        print(" ",end="");
+        
+    for b in range(a):
+        print("*",end=" ");
+        
+    print()
+
+rows = 5;
+for a in range(rows,0,-1):
+    
+    for s in range(0,rows - a):
+        print(" ",end=" ");
+        
+    for b in range(1,a+1):
+        print(b,end=" ");
+        
+    print()
+
+    
+rows = 5;
+for a in range(rows,0,-1):
+    
+    for s in range(0,rows - a):
+        print(" ",end=" ");
+        
+    for b in range(1,a+1):
+        print(a,end=" ");
+        
+    print()
+    
+    
+rows = 5;
+for a in range(rows,0,-1):
+    
+    for s in range(rows - a):
+        print(" ",end=" ");
+        
+    for b in range(a,0,-1):
+        print(b,end=" ");
+        
+    print()
+    
+    
+
---
+++ 30_pathern4.py
@@ -0,0 +1,47 @@
+rows=5;
+for a in range(rows):
+    for b in range(rows):
+        if a == 0 or a == rows - 1 or b == 0 or b == rows -1:
+            print("*",end=" ");
+        else:
+            print(" ",end=" ")
+    print()    
+        
+
+
+rows=5;
+for a in range(rows):
+    for s in range(rows -a -1):
+        print(" ",end="");
+        
+    for b in range(2 * a + 1):
+        print("*",end="")
+        
+    print()    
+        
+        
+rows=5;
+for a in range(rows):
+    for s in range(rows -a -1):
+        print(" ",end="");
+        
+    for b in range(2 * a + 1):
+        if b == 0 or b == 2 * a or a == rows-1:
+                print("*",end="");
+        else:
+            print(" ",end="");
+        
+    print()    
+
+
+rows=5;
+for a in range(rows):
+    for b in range(rows):
+        if a == b or a + b == rows-1:
+                print("*",end="");
+        else:
+            print(" ",end="");
+        
+    print()    
+        
+        
