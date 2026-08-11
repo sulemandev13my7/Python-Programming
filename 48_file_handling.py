2cc4c70d7b2d79c721e6bfe89d8074bd51a64be0
sulemandev13my7 <sulemanafandi18@gmail.com>
2026-08-11 20:23:13 +0500

    update

--- 48_file_handling.py
+++ 48_file_handling.py
@@ -2,4 +2,13 @@ f = open("students.txt", "r");
 
 
 # print(f.read());
+# print(f.read(5));
+# print(f.readline());
+# print(f.readline(),end="");
+# print(f.readlines());
+
+for line in f:
+    print(line.strip());
+    
+    
 f.close();
