bcf1664401739942473b3e6ebdc2fa203e55f83d
sulemandev13my7 <sulemanafandi18@gmail.com>
2026-08-12 06:54:11 +0500

    update

---
+++ 49_file_handling2.py
@@ -0,0 +1,80 @@
+# f = open("students.txt","w");
+
+# w = f.write("Hello Python")
+# f.close()
+
+# w = f.write("Hello Python\n")
+# w = f.write("Suleman\n")
+# w = f.write("Usman \n")
+# f.close()
+
+# print(w)
+
+
+
+
+# names = [
+#     "salman\n",
+#     "ayan\n",
+#     "salo\n",
+# ]
+
+# w = f.writelines(names)
+
+# f.close()
+
+
+
+# f = open("students.txt","a");
+
+# f.write("salman khan")
+
+# f.close()
+
+
+
+
+# f = open("students.txt","r+");
+
+# print(f.read())
+
+# f.write("\nPython")
+
+# f.close()
+
+
+
+
+
+# f = open("students.txt","w+");
+
+# f.write("\nBahi sahab")
+
+# f.seek(0)
+
+# print(f.read())
+
+# f.close()
+
+
+
+
+# f = open("students.txt","a+");
+
+# f.write("\nBahi")
+
+# f.seek(0)
+
+# print(f.read())
+
+# f.close()
+
+
+
+f = open("students1.txt","x");
+
+f.write("Python")
+
+f.close()
+
+
